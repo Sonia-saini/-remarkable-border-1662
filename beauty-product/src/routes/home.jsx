750d02231b325a39fec36778ed2473278724f65d
Sonia-saini <105914181+Sonia-saini@users.noreply.github.com>
2022-11-09 22:13:15 +0530

    yea

--- beauty-product/src/routes/home.jsx
+++ beauty-product/src/routes/home.jsx
@@ -1,4 +1,12 @@
 export default function Home(){
+    const Append=()=>{
+fetch(`http://localhost:8000/products`).then((res)=>
+     res.json()
+).then((res)=>(
+    console.log(res)
+))
+    }
+    Append()
     return (
         <>home
         </>
